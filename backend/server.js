ac15316bb174a9f724e996eabb64f1661f5edcea
Haneef Mohammad <haneefmohammed867@gmail.com>
2026-05-12 19:16:28 +0530

    Fixed Vercel CORS issue

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
+  'https://synora534.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
